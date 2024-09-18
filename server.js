fb8a97520a0f3a572237b6aa4f4af3b02a8ab99b
felipe-mendesg <141874115+felipe-mendesg@users.noreply.github.com>
2024-09-18 17:54:14 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,79 @@
+import express from "express";
+import cors from 'cors'
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const app = express();
+app.use(express.json());
+app.use(cors())
+
+app.post('/usuarios', async (req, res) => {
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  })
+
+
+  res.status(201).json(req.body);
+});
+
+app.get('/usuarios', async (req, res) => {
+  let users = []
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age
+      }
+    })
+  } else {
+    users = await prisma.user.findMany()
+  }
+
+  res.status(200).json(users);
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  });
+  res.status(201).json(req.body);
+});
+
+app.delete("/usuarios/:id", async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+
+  res.status(200).json({ message: "Usuario deletado com sucesso!" });
+});
+
+app.listen(3000);
+
+/*
+Criar nossa API de Usuários
+-Criar um usuario
+-Listar todos os usuarios
+-Editar um usuario
+-Deletar um usuario
+*/
+
+/*
+1) tipo de Rota / metodo HTTP
+2) Endereço
+*/
